e9728d9c0b696c802aa799914e2aa4026bb3f437
quangkhanh2221050711 <battleground.1710@gmail.com>
2024-05-21 20:44:18 +0700

    slide 60 chuong 4

---
+++ BaiiThucHanh/src/slide54.java
@@ -0,0 +1,8 @@
+import java.until.HashSet;
+improt java.until.Scanner;
+public class slide54 {
+    public static void main(String[] args){
+        
+    }
+    
+}
---
+++ BaiiThucHanh/src/slide60.java
@@ -0,0 +1,13 @@
+import java.until.LinkedHashSet;
+import java.util.Set;
+import java.unitl.set;
+public class slide60 {
+    public static void main(String [] args){
+        Set<String> linkedHashSet = new LinkedHashSet<String>();
+        linkedHashSet.add("HH");
+        linkedHashSet.add("AA");
+        linkedHashSet.add("BB");
+        linkedHashSet.add("CC");
+        linkedHashSet.remove("HH");
+    }
+}
