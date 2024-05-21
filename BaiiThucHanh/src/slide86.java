74f4a2681827987a1dd1d9cd65f4ee6c7922f1fc
quangkhanh2221050711 <battleground.1710@gmail.com>
2024-05-21 21:48:24 +0700

    slide 86 chuong 4

---
+++ BaiiThucHanh/src/slide86.java
@@ -0,0 +1,20 @@
+import java.out.hashmap;
+public class hashmap {
+
+    public static void main(String[] args) {
+        Hashmap<String,String> hashMap = new HashMap<>();
+         
+        hashMap.add("TMDT", "Thuong mai dien tu");
+        hashMap.add("JVCR","Java Crip");
+        hashMap.add("PT","Python");
+        hahsMap.add("C#","C sharp");
+         
+        Set<Map.Entry<String, String>> setHashMap = hashMap.entrySet();
+        System.out.pritnln("Cac entry co trong sethashmap la :");
+        System.out.println(setHashMap)
+
+
+
+    }
+    
+}
