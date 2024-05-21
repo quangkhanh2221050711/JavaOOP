172d4308a6f414e86cb292be3c79ffac33d9f451
quangkhanh2221050711 <battleground.1710@gmail.com>
2024-05-21 21:22:41 +0700

    slide 69 chuong 4

---
+++ BaiiThucHanh/src/Slide69.java
@@ -0,0 +1,21 @@
+import java.unitl.TreeSet;
+import java.util.Set;
+import java.until.Scanner;
+public class slide69 {
+    public static void main( String []args){
+        int songuyen;
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        Scanner scanner = new Scanner(System.in);
+
+        treeSetInteger.add("1");
+        treeSetInteger.add("2");
+        treeSetInteger.add("4");
+        treeSetInteger.add("8");
+        treeSetInteger.add("47");
+
+        System.out.printtln("Cac phan tu trong TreeSet la :");
+        System.out.println(treeSet);
+        System.out.println("Nhap phan tu can them :");
+        songuyen = scanner.nextInt();
+    }
+}
