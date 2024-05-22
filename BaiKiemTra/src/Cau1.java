92d6279d0d297d0bce8e6eda4befdabd12b3fce3
quangkhanh2221050711 <battleground.1710@gmail.com>
2024-05-22 10:02:04 +0700

    NQK711_Cau1

---
+++ BaiKiemTra/src/Cau1.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class NQK711Person {
+    Class Person{
+        String Ten;
+        Integer Diem;
+        Float ChieuCao;
+    }
+    void getTen(){
+        System.out.println("Nhap ten :");
+        Scanner scanner = new Scanner(System.in);
+        Ten = scanner next.String;
+        System.out.println("Ten cua person :"+Ten);
+    }
+    void getDiem(){
+        System.out.println("Nhap diem :");
+        Scanner scanner = new Scanner(System.in);
+        Diem = Scanner next.Integer;
+        System.out.println("Diem cua person :" +Diem)
+    }
+    void getChieuCao(){
+        System.out.println("Nhap chieu cao :");
+        Scanner scanner = new Scanner(System.in);
+        ChieuCao = Scanner next.Float;
+        System.out.println("ChieuCao cua person :" +ChieuCao)
+    }
+    public static void main(String[] args) {
+        LinkedList<String> linkedList = new LinkedList<>();
+
+    }
+}
